16b0ce09678e31e21c24baad251e5f0dd33de203
Antonio Russo <rjproj@gmail.com>
2019-06-24 18:27:03 +0200

    distribute approvals for erc20 benchmark init script

--- packages/caliper-application/src/contract/ethereum/erc20/init.js
+++ packages/caliper-application/src/contract/ethereum/erc20/init.js
@@ -1,4 +1,4 @@
-function init(mintableInstance, ethereumConfig) {
+async function init(mintableInstance, ethereumConfig) {
     let accountQuota = Math.floor(ethereumConfig.contracts.mintable.constructorArgs[0]/ethereumConfig.accounts.length);
     let promises = [];
     ethereumConfig.accounts.forEach(account => {
@@ -6,6 +6,18 @@ function init(mintableInstance, ethereumConfig) {
             from: ethereumConfig.contractDeployerAddress
         }));
     });
+    await Promise.all(promises);
+
+    promises = [];
+    allowanceQuota = Math.floor(accountQuota / (ethereumConfig.accounts.length * 2));
+    ethereumConfig.accounts.forEach(allower => {
+        ethereumConfig.accounts.forEach(spender => {
+            promises.push(mintableInstance.methods.transfer(spender.address, accountQuota).send({
+                from: allower.address
+            }));
+        });
+    });
+
     return Promise.all(promises)
 }
 
