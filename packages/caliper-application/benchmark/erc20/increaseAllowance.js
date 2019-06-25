8f0d171138deacc2bbc03d385df8246d3ddc4ae8
Antonio Russo <rjproj@gmail.com>
2019-06-25 15:33:40 +0200

    Provide ERC20 functions in different workloads

--- packages/caliper-application/benchmark/erc20/increaseAllowance.js
+++ packages/caliper-application/benchmark/erc20/increaseAllowance.js
@@ -6,16 +6,16 @@ const logger = CaliperUtils.getLogger('transfer.js');
 module.exports.info  = 'transfer money';
 
 let account_array = [];
-let transfers = {};
+let approvals = {};
 let txnPerBatch;
-let transferMoney;
+let allowerMoney;
 let bc, contx;
 
 module.exports.init = function(blockchain, context, args) {
-    if(!args.hasOwnProperty('transferMoney')) {
-        return Promise.reject(new Error('mintable.transfer - \'transferMoney\' is missed in the arguments'));
+    if(!args.hasOwnProperty('allowerMoney')) {
+        return Promise.reject(new Error('mintable.approve - \'allowerMoney\' is missed in the arguments'));
     }
-    transferMoney = args.transferMoney;
+    allowerMoney = args.allowerMoney;
 
     if(!args.hasOwnProperty('txnPerBatch')) {
         args.txnPerBatch = 1;
@@ -26,8 +26,8 @@ module.exports.init = function(blockchain, context, args) {
     contx = context;
 
     blockchain.bcObj.ethereumConfig.accounts.forEach(account => {
-        account_array.push(account.address)
-        transfers[account.address] = 0
+        account_array.push(account.address);
+        approvals[account.address] = 0;
     });
 
     return Promise.resolve()
@@ -38,22 +38,22 @@ function getRandomAccount() {
     return account_array[Math.floor(Math.random() * account_array.length)]
 }
 
-function generateTransfer() {
+function generateApprove() {
     let receiver = getRandomAccount();
     return {
-        'verb': 'transfer',
-        'args': [receiver, transferMoney]
+        'verb': 'increaseAllowance',
+        'args': [receiver, allowerMoney]
     }
 }
 
 module.exports.run = function() {
-    let transfer = generateTransfer()
-    transfers[transfer.args[0]] += transfer.args[1];
-    return bc.invokeSmartContract(contx, 'mintable', 'v0', [transfer], 100);
+    let approve = generateApprove();
+    approvals[approve.args[0]] = approve.args[1];
+    return bc.invokeSmartContract(contx, 'mintable', 'v0', [approve], 100);
 };
 
 module.exports.end = function() {
     return Promise.resolve();
 };
 
-module.exports.transfers = transfers;
+module.exports.approvals = approvals;
---
+++ packages/caliper-application/benchmark/erc20/transferFrom.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const {BlockchainInterface, CaliperUtils, TxStatus} = require('caliper-core');
+const logger = CaliperUtils.getLogger('transferFrom.js');
+
+module.exports.info  = 'transfer money';
+
+let account_array = [];
+let allowed = {};
+let transfered = {};
+let txnPerBatch;
+let allowerMoney;
+let bc, contx;
+
+module.exports.init = async function(blockchain, context, args) {
+    if(!args.hasOwnProperty('allowerMoney')) {
+        return Promise.reject(new Error('mintable.transfer - \'allowerMoney\' is missed in the arguments'));
+    }
+    allowerMoney = args.allowerMoney;
+
+    if(!args.hasOwnProperty('txnPerBatch')) {
+        args.txnPerBatch = 1;
+    }
+    txnPerBatch = args.txnPerBatch;
+
+    bc = blockchain;
+    contx = context;
+
+    for (let i=0; i<blockchain.bcObj.ethereumConfig.accounts.length; i++) {
+        let account = blockchain.bcObj.ethereumConfig.accounts[i].address;
+        account_array.push(account);
+        let queryAllowance = await bc.queryState(contx, 'mintable', 'v0', [account, blockchain.bcObj.ethereumConfig.fromAddress], 'allowance');
+        allowed[account] = queryAllowance.GetResult();
+    }
+    
+    return Promise.resolve();
+    
+};
+
+function getRandomAccount() {
+    return account_array[Math.floor(Math.random() * account_array.length)]
+}
+
+function generateTransferFrom() {
+    let receiver = getRandomAccount();
+    
+    let granter = getRandomAccount();
+    while (allowed[granter] <= 0) {
+        granter = getRandomAccount();
+    }
+
+    return {
+        'verb': 'transferFrom',
+        'args': [granter, receiver, allowerMoney]
+    };
+}
+
+module.exports.run = function() {
+    return bc.invokeSmartContract(contx, 'mintable', 'v0', [generateTransferFrom()], 100);
+};
+
+module.exports.end = function() {
+    return Promise.resolve();
+};
+
+module.exports.account_array = account_array;
