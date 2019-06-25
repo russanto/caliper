281db0ce6a57edf36eefc7f27ecd501befa87dbd
Antonio Russo <rjproj@gmail.com>
2019-06-25 17:28:48 +0200

    Eliminate allowance init from all accounts in ERC20

--- packages/caliper-application/src/contract/ethereum/erc20/init.js
+++ packages/caliper-application/src/contract/ethereum/erc20/init.js
@@ -6,19 +6,7 @@ async function init(mintableInstance, ethereumConfig) {
             from: ethereumConfig.contractDeployerAddress
         }));
     });
-    await Promise.all(promises);
-
-    promises = [];
-    allowanceQuota = Math.floor(accountQuota / (ethereumConfig.accounts.length * 2));
-    ethereumConfig.accounts.forEach(allower => {
-        ethereumConfig.accounts.forEach(spender => {
-            promises.push(mintableInstance.methods.transfer(spender.address, accountQuota).send({
-                from: allower.address
-            }));
-        });
-    });
-
-    return Promise.all(promises)
+    return Promise.all(promises);
 }
 
 module.exports.init = init
